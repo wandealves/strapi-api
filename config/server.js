0032b9ce4000cd7a1299584e6e93e56f6af540c1
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-10-09 15:12:11 -0300

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a4ac0beffb9ba1159dff9a559999ae49'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ef70f41-8c22-4e46-8c55-d822e8ef87c0'
+};
